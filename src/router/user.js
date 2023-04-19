9bd0ca4718c36cfaf9b03eef96436368f701af29
Kyoungjin Tak <kyoungjintak@Kyoungjinui-MacBookPro.local>
2023-04-19 18:06:45 +0900

    isAuth

--- src/router/user.js
+++ src/router/user.js
@@ -1,13 +1,40 @@
 const express = require('express');
-const { signUp, login, test } = require('./controller/userController');
+
+const { User } = require('../../src/dao/userdao/mongoose/model');
+const hashedPassword = require('../../src/utils/hash');
 const userRouter = express.Router();
+const { generateToken } = require('../../src/utils/token');
 
 //회원가입
-userRouter.post('/signup', signUp);
+userRouter.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+  const newUser = await User.create({
+    email,
+    password: hashedPassword(password),
+  });
+  res.send(newUser);
+});
 
 //로그인
-userRouter.post('/login', login);
-
-userRouter.get('/test', test);
+userRouter.post('/login', async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.status(401).send({
+      message: 'User Not Found',
+    });
+  }
+  if (user.password !== hashedPassword(password)) {
+    res.status(401).send({
+      message: 'Invalid Password',
+    });
+  }
+  const payload = {
+    _id: user._id,
+    email,
+  };
+  const token = generateToken(payload);
+  res.send(token);
+});
 
 module.exports = userRouter;
