6dc9417acd507566cd0bf8b980c2b3116347f198
[SW4] 강민석 <bbde1861@gmail.com>
2023-04-24 22:06:32 +0900

    [fix] port fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const app = require('./app.js');
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${process.env.PORT}`);
+  console.log(`Server is listening on port ${3000}`);
 });
