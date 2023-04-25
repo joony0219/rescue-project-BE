223eec99edc320be42844fb08ed09b03b9abcb0b
Kyoungjin Tak <kyoungjintak@Kyoungjinui-MacBookPro.local>
2023-04-26 00:08:24 +0900

    order

--- src/router/order/order_router.js
+++ src/router/order/order_router.js
@@ -2,15 +2,21 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const orderController = require('../../controller/order_controller.js');
-const SoldProduct = require("../../dao/soldproductdao/mongoose/model/soldproduct_model.js");
-const { validateOrder } = require("../../middleware/validate/schema/order_validate.js");
+const SoldProduct = require('../../dao/soldproductdao/mongoose/model/soldproduct_model.js');
+const UserDAO = require('../../dao/userdao/userDAO.js');
+const {
+  validateOrder,
+} = require('../../middleware/validate/schema/order_validate.js');
 
 // /order/
 
 // user order
 router.post(
   '/',
-  passport.authenticate('http-only-cookie-use-user', { session: false, failWithError: true }),
+  passport.authenticate('http-only-cookie-use-user', {
+    session: false,
+    failWithError: true,
+  }),
   validateOrder,
   orderController.postOrder
 );
@@ -23,8 +29,9 @@ router.get(
     failWithError: true,
   }),
   async (req, res) => {
-    const userName = req.user.userName;
-    const soldProduct = await SoldProduct.find({ userName });
+    const user = await UserDAO.findByUserName(userName);
+    const userId = user._id;
+    const soldProduct = await SoldProduct.find({ userId });
     return res.status(200).json(buildResponse(null, soldProduct));
   }
 );
