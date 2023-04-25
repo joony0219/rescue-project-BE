1cfc57460f597c1878d773484a6ad4d7de4b3774
Kyoungjin Tak <kyoungjintak@Kyoungjinui-MacBookPro.local>
2023-04-25 16:57:31 +0900

    orders

--- src/router/order/order_router.js
+++ src/router/order/order_router.js
@@ -6,10 +6,24 @@ const orderController = require('../../controller/order_controller.js');
 //상품주문하기
 router.post(
   '/orders',
-  passport.authenticate('http-only-cookie-use-user', { session: false, failWithError: true }),
-  
-
+  passport.authenticate('http-only-cookie-use-user', {
+    session: false,
+    failWithError: true,
+  })
 );
 
+//구매 상품 조회
+router.get(
+  '/orders',
+  passport.authenticate('http-only-cookie-use-user', {
+    session: false,
+    failWithError: true,
+  }),
+  async (req, res) => {
+    const userName = req.user.userName;
+    const soldProduct = await SoldProduct.find({ userName });
+    return res.status(200).json(buildResponse(null, soldProduct));
+  }
+);
 
 module.exports = router;
